d5185a0a130c9039e17514cabc28bedcc0c95886
GustavoMatsunaga <63619508+GustavoMatsunaga@users.noreply.github.com>
2020-09-24 16:10:16 -0300

    Updates

---
+++ precache-manifest.6ca09c7a8dba05c3dbc79608a1c2e3d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "737194a7162bc54a3942ba3d69b749e9",
+    "url": "/ToDoListReact/index.html"
+  },
+  {
+    "revision": "8b2d6fcfa5f3808f0b9e",
+    "url": "/ToDoListReact/static/css/main.b08f8842.chunk.css"
+  },
+  {
+    "revision": "9adce8b3d149d67b33c2",
+    "url": "/ToDoListReact/static/js/2.1a901f02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoListReact/static/js/2.1a901f02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b2d6fcfa5f3808f0b9e",
+    "url": "/ToDoListReact/static/js/main.147824a9.chunk.js"
+  },
+  {
+    "revision": "c2686f9d9da73956a800",
+    "url": "/ToDoListReact/static/js/runtime-main.a2110f78.js"
+  }
+]);
